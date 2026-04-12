66db8447947b04899129dfde5f647b7fdff0c4c6
Ahmed Hindy <Ahmed.hindy96@gmail.com>
2026-04-12 17:23:39 +0200

    Improve random item spawning and filtering

--- src/RandomItems.h
+++ src/RandomItems.h
@@ -10,6 +10,7 @@
 class RandomItems : public IPluginInterface {
 public:
     void OnEngineInitialized() override;
+    RandomItems();
     ~RandomItems() override;
     void OnDrawMenu() override;
     void OnDrawUI(bool p_HasFocus) override;
@@ -20,12 +21,12 @@ private:
     void GiveRandomItem();
     std::string ConvertDynamicObjectValueTString(const ZDynamicObject& p_DynamicObject);
 
-private:
     double m_ElapsedTime = 0;
     double m_DelaySeconds = 15;
     bool m_Running = false;
     bool m_ShowMessage = false;
     bool m_SpawnInWorld = true;
+    bool m_AICanSeeItems = false;
     bool m_IncludeItemsWithoutTitle = false;
     float m_HitmanItemPosition[3] = { 0, 1, 0 };
     TResourcePtr<ZTemplateEntityFactory> m_RepositoryResource;
@@ -33,6 +34,17 @@ private:
 
     std::mt19937 m_RandomGenerator;
     std::uniform_int_distribution<size_t> m_Distribution;
+
+    // Full list of categories the user can toggle
+    const std::vector<std::string> m_AllCategories {
+        "assaultrifle", "sniperrifle", "melee",       "explosives",
+        "tool",         "pistol",      "shotgun",     "suitcase",
+        "smg",          "distraction", "poison",      "container"
+    };
+
+    // One flag per category; true means enabled.
+    // Initialized to all true by the constructor.
+    std::vector<bool> m_CategoryEnabled;
 };
 
 DEFINE_ZHM_PLUGIN(RandomItems)
